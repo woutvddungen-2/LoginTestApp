c1730ddec19b390e7f1112dc40ba4371e507ae97
Wout Dungen <320117@student.fontys.nl>
2025-11-06 13:47:03 +0100

    changed gitignore to ignore migration data

--- Server/Data/AppDbContext.cs
+++ Server/Data/AppDbContext.cs
@@ -17,7 +17,7 @@ namespace Server.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // Composite key for GroupMember
+            // Composite key for ChatGroupMember
             modelBuilder.Entity<ChatGroupMember>()
                 .HasKey(gm => new { gm.UserId, gm.ChatGroupId });
 
@@ -41,6 +41,52 @@ namespace Server.Data
                 .HasOne(m => m.Group)
                 .WithMany(g => g.Messages)
                 .HasForeignKey(m => m.GroupId);
+
+            // ======== SEED DATA ========
+
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Username = "Alice", PasswordHash = "MOfFeyR+dJdD998daoFvZjlS3Zc9N6POz65+RRARhoOgX4pCTNvWCTIANdj9ti6p" },
+                new User { Id = 2, Username = "Bob", PasswordHash = "MOfFeyR+dJdD998daoFvZjlS3Zc9N6POz65+RRARhoOgX4pCTNvWCTIANdj9ti6p" },
+                new User { Id = 3, Username = "Charlie", PasswordHash = "MOfFeyR+dJdD998daoFvZjlS3Zc9N6POz65+RRARhoOgX4pCTNvWCTIANdj9ti6p" },
+                new User { Id = 4, Username = "Joseph", PasswordHash = "MOfFeyR+dJdD998daoFvZjlS3Zc9N6POz65+RRARhoOgX4pCTNvWCTIANdj9ti6p" },
+                new User { Id = 5, Username = "Diana", PasswordHash = "MOfFeyR+dJdD998daoFvZjlS3Zc9N6POz65+RRARhoOgX4pCTNvWCTIANdj9ti6p" }
+            );
+
+            modelBuilder.Entity<ChatGroup>().HasData(
+                new ChatGroup { Id = 1, Name="random", CreatedAt = DateTime.UtcNow },
+                new ChatGroup { Id = 2, Name="software", CreatedAt = DateTime.UtcNow },
+                new ChatGroup { Id = 3, Name="Testing",CreatedAt = DateTime.UtcNow.AddDays(-1) },
+                new ChatGroup { Id = 4, Name="Movies",CreatedAt = DateTime.UtcNow.AddDays(-1) }
+            );
+
+            modelBuilder.Entity<ChatGroupMember>().HasData(
+                new ChatGroupMember { UserId = 1, ChatGroupId = 1, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 2, ChatGroupId = 1, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 3, ChatGroupId = 1, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 4, ChatGroupId = 1, JoinedAt = DateTime.UtcNow },
+
+                new ChatGroupMember { UserId = 2, ChatGroupId = 2, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 1, ChatGroupId = 2, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 4, ChatGroupId = 2, JoinedAt = DateTime.UtcNow },
+                new ChatGroupMember { UserId = 5, ChatGroupId = 2, JoinedAt = DateTime.UtcNow },
+
+                new ChatGroupMember { UserId = 4, ChatGroupId = 3, JoinedAt = DateTime.UtcNow.AddDays(-12) },
+                new ChatGroupMember { UserId = 4, ChatGroupId = 4, JoinedAt = DateTime.UtcNow.AddDays(-1) }
+            );
+
+            modelBuilder.Entity<Message>().HasData(
+                new Message { Id = 1, GroupId = 1, SenderId = 1, Content = "Hi Bob!", CreatedAt = DateTime.UtcNow },
+                new Message { Id = 2, GroupId = 1, SenderId = 2, Content = "Hey Alice!", CreatedAt = DateTime.UtcNow },
+                new Message { Id = 3, GroupId = 1, SenderId = 2, Content = "Hello Charlie!", CreatedAt = DateTime.UtcNow },
+                new Message { Id = 4, GroupId = 2, SenderId = 1, Content = "Hi Bob!", CreatedAt = DateTime.UtcNow },
+                new Message { Id = 5, GroupId = 2, SenderId = 2, Content = "Hey Alice!", CreatedAt = DateTime.UtcNow },
+                new Message { Id = 6, GroupId = 2, SenderId = 2, Content = "Hello Charlie!", CreatedAt = DateTime.UtcNow }
+            );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, UserId = 1, Name = "Apple", Price = 5 },
+                new Product { Id = 2, UserId = 2, Name = "Apple", Price = 5 }
+            );
         }
     }
 }
